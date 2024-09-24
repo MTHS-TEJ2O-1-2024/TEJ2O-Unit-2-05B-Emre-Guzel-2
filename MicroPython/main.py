bf84336978b2b9a2e78b8c76c73fbbd0900ed93a
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-24 21:32:03 +0000

    made the Kelvin tempurature using python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,12 +1,16 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module is a Micro:bit MicroPython measures the temperature of Kelvin   
 """
 
 from microbit import *
 
+display.clear()
+
+display.show(Image.HAPPY)
+
 while True:
     if button_a.is_pressed():
-        temperature = temperature()
-        display.scroll("The temperature is " + str(temperature) + "C")
+        temperature = temperature() + round(273.15)
+        display.scroll("The temperature is: " + str(temperature) + "K")
