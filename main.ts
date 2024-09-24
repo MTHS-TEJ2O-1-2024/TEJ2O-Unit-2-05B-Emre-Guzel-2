d884f190a8f88a026fb42d7c66c0249bf8e46acc
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-24 10:49:52 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Sep 24 2024
+ * This program meuseures the tempurature of Kelvin 
 */
 
 basic.showString('Hello, World!')
